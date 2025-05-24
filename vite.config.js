f200bb25c6c1ba29e8038a9445257fe281017741
Tanu kumari <168282696+tanu2408@users.noreply.github.com>
2025-05-24 18:49:07 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // enables listening on 0.0.0.0
+    allowedHosts: ['weatherapp-1-vspg.onrender.com'], // allow your Render hostname
+    port: 5173, // optional, specify if needed
+  }
 })
